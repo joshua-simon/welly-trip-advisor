fc948ad600ddc271c77431fb7cbd99f28b528cbb
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-08 15:04:48 +1300

    options bar added

--- src/options.js
+++ src/options.js
@@ -1,8 +1,19 @@
 
 const Options = () => {
     return(
-        <div>
-            this is the Options
+        <div className = 'options-container'>
+            <div className = 'options-item'>
+                <p>Restaurants</p>
+                <img src="https://img.icons8.com/ios/50/000000/restaurant.png"/>
+            </div>
+            <div className = 'options-item'>
+                <p>Bars</p>
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/beer.png"/>
+            </div>
+            <div className = 'options-item'>
+                <p>Cafes</p>
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/coffee.png"/>
+            </div>
         </div>
     )
 }
