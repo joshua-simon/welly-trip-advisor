0b55d0ff90f071a92cad7721fc6e57ed3b7fc577
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-14 16:15:06 +1300

    added header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,11 +40,13 @@ const Header = () => {
                       d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                     />
                     </svg>
-                    </>
-                    <button>Login</button>
-                    <Link to = '/signup'>
-                      <button>Sign up</button>
+                    <Link to = '#'>
+                      Login
                     </Link>
+                    <Link to = '/signup'>
+                      Register
+                    </Link>
+                    </>
                 </div>
             </div>
         </header>
